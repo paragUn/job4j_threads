d9a7b0150db8015c00f9ba2ce89b806ac7aa3387
Roman <roman_stalmakov@mail.ru>
2023-10-31 23:40:49 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        try {
+            for (int index = 0; index < 4; index++) {
+                System.out.print("\rLoading ... " + process[index]);
+                Thread.sleep(500);
+                index = (index >= 3) ? -1 : index;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000);
+            progress.interrupt();
+    }
+}
