5bd0d1d306eba72062ea513cb454c2f24d6a0ca0
Roman <roman_stalmakov@mail.ru>
2023-11-02 00:41:19 +0300

    3. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,8 +1,9 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
+    public static final Thread.State TERM = Thread.State.TERMINATED;
+
     public static void main(String[] args) {
-        var term = Thread.State.TERMINATED;
         Thread first = new Thread(
                 () -> { }
         );
@@ -12,13 +13,11 @@ public class ThreadState {
 
         first.start();
         System.out.println(first.getName() + " - " + first.getState());
-        while (first.getState() != term) {
-            System.out.println(first.getName() + " - " + first.getState());
-        }
-
         second.start();
         System.out.println(second.getName() + " - " + second.getState());
-        while (second.getState() != term) {
+
+        while (first.getState() != TERM && second.getState() != TERM) {
+            System.out.println(first.getName() + " - " + first.getState());
             System.out.println(second.getName() + " - " + second.getState());
         }
 
